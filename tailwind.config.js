7b59c71d19203db1ba44fc48bfaafdc401c9cbcf
Sanskar2001 <sanskaratrey22@gmail.com>
2025-10-17 23:23:45 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: '#4757FF',
+          dark: '#3846CC',
+        },
+      },
+      boxShadow: {
+        card: '0 1px 2px rgba(16,24,40,0.06), 0 1px 3px rgba(16,24,40,0.10)',
+      },
+      borderRadius: {
+        xl: '14px',
+      },
+    },
+  },
+  plugins: [],
+}
